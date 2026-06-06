fb9c8b4dd55f9c9e4f096e812f761e831a10fa03
Suke2004 <lyricsofsongs96@gmail.com>
2026-06-06 22:42:20 +0530

    login endpoint added

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,7 +4,6 @@ const jwt = require("jsonwebtoken");
  * - user register controller
  * - POST /api/auth/register
  */
-
 async function userRegisterController(req, res) {
   if (!req.body || Object.keys(req.body).length === 0) {
     return res.status(400).json({
@@ -46,6 +45,42 @@ async function userRegisterController(req, res) {
   });
 }
 
+/*
+ * - user login controller
+ * - POST /api/auth/login
+ */
+async function userLoginController(req, res) {
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email }).select("+password");
+
+  if (!user) {
+    return res.status(401).json({
+      message: "Email or Password is INVALID",
+    });
+  }
+
+  const isValidPassword = await user.comparedPassword(password);
+
+  if (!isValidPassword) {
+    return res.status(401).json({
+      message: "Email or Password is INVALID",
+    });
+  }
+  const token = jwt.sign({ userID: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "3d",
+  });
+  res.cookie("token", token);
+  res.status(200).json({
+    user: {
+      _id: user._id,
+      email: user.email,
+      name: user.name,
+    },
+    token,
+  });
+}
+
 module.exports = {
   userRegisterController,
+  userLoginController,
 };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 /* POST /api/auth/register */
 router.post("/register", authController.userRegisterController);
+router.post("/login", authController.userLoginController);
 
 
 module.exports = router;
